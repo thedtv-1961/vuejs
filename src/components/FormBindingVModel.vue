ee82ad9e693c70a5e1057e2abf6ec47372a31ce4
Đàm Thế <dam.trieu.van.the@sun-asterisk.com>
2022-03-02 21:05:48 +0700

    Form binding v-model

---
+++ src/components/FormBindingVModel.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  data() {
+    return {
+      txt: "",
+    };
+  },
+};
+</script>
+
+<template>
+  <input type="text" v-model="txt" placeholder="Input text" />
+  <p>{{ txt }}</p>
+</template>
