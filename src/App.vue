bfce5fddd06e1536fbe4a29436ee929260a9d917
Đàm Thế <dam.trieu.van.the@sun-asterisk.com>
2022-03-02 20:55:34 +0700

    Clear

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <script>
 export default {
-  data() {
-    return {
-      counter: 0,
-    };
-  },
-  methods: {
-    increment() {
-      return this.counter++;
-    }
-  }
+  
 };
 </script>
 
 <template>
-  <button @click="increment">Counter = {{ counter }}</button>
+  
 </template>
