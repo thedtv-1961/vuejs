b46187152b70e982ff63b174819b33e28b4ed496
Đàm Thế <dam.trieu.van.the@sun-asterisk.com>
2022-03-03 18:04:21 +0700

    Emit

---
+++ src/components/Emit.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="btnClick">Call parent method from child component</button>
+</template>
+
+<script>
+export default {
+  emits: ["parentMethod"],
+  methods: {
+		btnClick() {
+			this.$emit('parentMethod', 'abc');
+		}
+	},
+};
+</script>
