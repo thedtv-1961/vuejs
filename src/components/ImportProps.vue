583b8e3887db19ad69a09e412046c3aa8019e214
Đàm Thế <dam.trieu.van.the@sun-asterisk.com>
2022-03-03 15:08:16 +0700

    Props

---
+++ src/components/ImportProps.vue
@@ -0,0 +1,13 @@
+<script>
+import Props from './components/Props.vue';
+
+export default {
+ components: {
+	 Props
+ },
+};
+</script>
+
+<template>
+ <Props :msg="'abc'" />
+</template>
---
+++ src/components/Props.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>Value of props = {{ msg || 'no value' }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+};
+</script>
